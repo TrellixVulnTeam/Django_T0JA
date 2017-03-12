704c624a30af4d8a6f8cb0f998ee99a716446cde
Arjun  Dass <ad.sam007@gmail.com>
2017-03-11 23:39:31 -0500

    Adding updated folder

--- djangular/djangular/scrumboard/api.py
+++ djangular/djangular/scrumboard/api.py
@@ -1,13 +1,14 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 from .serializers import ListSerializer, CardSerializer
 from .models import List, Card
 
 # These are view classes. They are basically the components that goes through the request by the user and will retrive the data it need JSON. and will send it back
 
-class ListApi(ListAPIView):
+class ListViewSets(ModelViewSet):
     queryset = List.objects.all()                           # List is List models class. Object = query get all() will give back the list of python objects
     serializer_class = ListSerializer                       # point the listView to listSerializer
 
-class CardApi(ListAPIView):
+class CardViewSets(ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+
--- djangular/djangular/scrumboard/serializers.py
+++ djangular/djangular/scrumboard/serializers.py
@@ -4,15 +4,16 @@ from .models import List, Card
 # rest framework is used to send data from backend to front end in JSON
 # serializers converts these data into JSON
 # ModelSerializer will look up your module, will look up their fields and convert them to JSON
-
-class ListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = List
-        fields = '__all__'
-
 class CardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Card
         fields = '__all__'
+
+class ListSerializer(serializers.ModelSerializer):
+    cards = CardSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = List
+        fields = '__all__'
+
