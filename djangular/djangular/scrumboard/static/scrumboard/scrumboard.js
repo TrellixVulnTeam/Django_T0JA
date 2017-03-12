de67c4de6a1f4dedff8a89eb042e0b2d5daca243
Arjun  Dass <ad.sam007@gmail.com>
2017-03-11 23:38:46 -0500

    Added new modules

--- djangular/djangular/scrumboard/static/scrumboard/scrumboard.js
+++ djangular/djangular/scrumboard/static/scrumboard/scrumboard.js
@@ -1,43 +1,26 @@
 (function(){
                 'use strict';
-                angular.module('scrumboard.demo', [])
-                    .controller('ScrumboardController', [ '$scope', ScrumboardController]);
+                angular.module('scrumboard.demo', [])   //we are creating a module, that's y an extra parameter
+                    .controller('ScrumboardController', [ '$scope', '$http',ScrumboardController]); //DEPENDENCIES - https and function
 
-                function ScrumboardController($scope){
+                function ScrumboardController($scope, $http){
 
                         $scope.add = function (list, title) {
                             var card = {
+                                list: list.id,
                                 title: title
                             };
-                            list.cards.push(card)
+                            $http.post('scrumboard/cards/', card).then(function(response){ //2nd arg is the card object created earlier
+                                list.cards.push(response.data);
+                            }, function(){
+                                alert('Could not create Card');
+                            });
                         };
 
-                        $scope.data = [
-                            {
-                                name: 'Django Demo',
-                                cards: [
-                                    {
-                                        title: 'Create Models'
-                                    },
-                                    {
-                                        title: 'Create View'
-                                    },
-                                    {
-                                        title: 'Migrate Database'
-                                    },
-                                ]
-                            },{
-                                name: 'Angular Demo',
-                                cards: [
-                                    {
-                                        title: 'Write HTML'
-                                    },
-                                    {
-                                        title: 'Write Javascript'
-                                    },
-                                ]
-                            }
-                        ];
+                        $scope.data = [];                                   //fill it with data from the database
+                        $http.get('/scrumboard/lists').then(function(response){     //this is an asynchronous call, doesn't wait for data to return from the server
+                            $scope.data = response.data;                    //it automatically takes a return in a promise form
+                        });
                 }
 
             }());
