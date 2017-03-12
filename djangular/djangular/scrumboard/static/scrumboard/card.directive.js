0a59754adb11bf9658471505a321015ef8100d32
Arjun  Dass <ad.sam007@gmail.com>
2017-03-12 16:23:40 -0500

    Added update and delete function

--- djangular/djangular/scrumboard/static/scrumboard/card.directive.js
+++ djangular/djangular/scrumboard/static/scrumboard/card.directive.js
@@ -6,7 +6,27 @@
     function CardDirective(){
         return {
             templateUrl: '/static/scrumboard/card.html',
-            restrict: 'E'
+            restrict: 'E',
+            controller: ['$scope', '$http', function($scope, $http) {
+                var url ='/scrumboard/cards/' + $scope.card.id + '/';
+                $scope.update = function(){
+                    $http.put(
+                        url,
+                        $scope.card
+                    );
+                };
+
+                $scope.delete = function(){
+                    $http.delete(url).then(function(){
+                        var cards = $scope.list.cards;
+                        cards.splice(cards.indexOf($scope.card),       //splice because js doesn't provide a mechanism to remove an element form an array
+                         1
+                        );
+                    }, function(){
+                        alert('Could not delete Card');
+                    });
+                };
+            }]
         };
     }
 })();
