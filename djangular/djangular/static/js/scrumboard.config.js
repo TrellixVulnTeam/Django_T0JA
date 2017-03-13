75a3929ca48a33880f47e267f81493342193e5a9
Arjun  Dass <ad.sam007@gmail.com>
2017-03-13 18:12:02 -0500

    Added a nre config file

--- djangular/djangular/static/js/scrumboard.config.js
+++ djangular/djangular/static/js/scrumboard.config.js
@@ -1,10 +1,16 @@
 (function(){
     'use strict';
 
-    angular.module('scrumboard.demo').run(['$http', run]);      //any function in angular run will run when the application starts after loading all modules
-
+    angular.module('scrumboard.demo').config(['$routeProvider', config]).run(['$http', run]);      //any function in angular run will run when the application starts after loading all modules
+    
+    function  config($routeProvider) {
+        $routeProvider.when('/', {templateUrl: '/static/html/scrumboard.html', controller: 'ScrumboardController',})
+                      .when('/login', {templateUrl:'/static/html/login.html', controller: 'LoginController'})
+                      .otherwise('/');
+    }
+    
     function run($http){
         $http.defaults.xsrfHeaderName = 'X-CSRFToken';
-        $http.default.xsrfCookieName =  'csrftoken';
+        $http.defaults.xsrfCookieName =  'csrftoken';
     }
 })();
